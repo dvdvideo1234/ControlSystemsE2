87ef9814dff807d69f698794b41917b5665c3cd2
Deyan Dobromirov <dvd_video@abv.bg>
2018-07-26 15:51:18 +0300

    Added: Wiki extractor

---
+++ data/wiki/wiki_extract.lua
@@ -0,0 +1,88 @@
+local common = require('common')
+common.addPathLibrary("E:/Documents/Lua-Projs/ZeroBraineIDE/myprograms/wiki-extract","*.lua")
+local wikilib = require('wikilib')
+
+local API = {
+  NAME = "StControl",
+  POOL = {
+    {name="MAKE",cols={"Instance.creator", "Out", "Description"},size={50,5,13}},
+    {name="APPLY",cols={"Class.methods", "Out", "Description"},size={50,5,13}},
+  },
+  FILE = {
+    base = "E:/Documents/Lua-Projs/SVN/ControlSystemsE2/",
+    path = "data/wiki",
+    slua = "lua/entities/gmod_wire_expression2/core/custom"
+  },
+  TYPE = {
+    E2 = "stcontrol",
+    __obj = "xsc",
+    __pic = false,
+    __non = "xxx", -- None or missed type.. aka VOID
+    __tfm = "type-%s.jpg",
+    __rty = "ref-%s",
+    __rbr = "[ref-%s]: %s",
+    __ref = "![ref-%s]: %s",
+    __img = "![image][%s]",
+    link = "https://raw.githubusercontent.com/dvdvideo1234/ControlSystemsE2/master/data/pictures/types/%s",
+    list = {
+      {"a", "Angle"},
+      {"e", "Entity"},
+      {"n", "Number"},
+      {"r", "Array"},
+      {"s", "String"},
+      {"v", "Vector 3D"},
+      {"xv2", "Vactor 2D"},
+      {"xfs", "Flash sensor class"},
+      {"xsc", "State controller class"},
+      {"xxx", "Void value "}
+    },
+    idx = {
+      ["angle"]     = 1,
+      ["entity"]    = 2,
+      ["number"]    = 3,
+      ["array"]     = 4,
+      ["string"]    = 5,
+      ["vector"]    = 6,
+      ["vector2"]   = 7,
+      ["fsensor"]   = 8,
+      ["stcontrol"] = 9,
+      ["void"]      = 10
+    }
+  },
+
+  REPLACE = {
+    __key = "###", -- The key tells what patternis to be raplaced
+    ["MASK"] = "[###](https://wiki.garrysmod.com/page/Enums/###)",
+    ["COLLISION_GROUP"] = "[COLLISION_GROUP](https://wiki.garrysmod.com/page/Enums/###)"
+  }
+}
+
+API.RETURN = {
+  PREF = {
+    ["dump"] = API.TYPE.__obj,
+    ["res"] = API.TYPE.__obj,
+    ["set"] = API.TYPE.__obj,
+    ["upd"] = API.TYPE.__obj,
+    ["smp"] = API.TYPE.__obj,
+    ["add"] = API.TYPE.__obj,
+    ["rem"] = API.TYPE.__obj,
+    ["no"]  = API.TYPE.__obj,
+    ["new"] = API.TYPE.__obj,
+    ["is"] = "n"
+  },
+  MATCH = {}
+}
+
+local E2Helper = {}
+E2Helper.Descriptions = {}
+
+------------------------------------------------------PUT E2 DESCRIPTION HERE------------------------------------------------------
+
+------------------------------------------------------PUT E2 DESCRIPTION HERE------------------------------------------------------
+
+wikilib.updateAPI(API, DSC)
+wikilib.makeReturnValues(API)
+wikilib.printDescriptionTable(API, DSC, 1)
+wikilib.printDescriptionTable(API, DSC, 2)
+wikilib.printTypeTable(API)
+wikilib.printTypeReference(API)
---
+++ data/wiki/wikilib.lua
@@ -0,0 +1,214 @@
+local common = require('common')
+
+local wikilib = {}
+
+local function apiSortFinctionParam(a, b)
+  if(table.concat(a.par) < table.concat(b.par)) then return true end
+  return false
+end
+
+function wikilib.updateAPI(API, DSC)
+  local t = API.POOL[1]
+  for n in pairs(DSC) do
+    if(n:find(API.NAME)) then t = API.POOL[1] else t = API.POOL[2] end
+    DSC[n] = DSC[n]:gsub("/","`")
+    for k in pairs(API.REPLACE) do
+      if(DSC[n]:find(k)) then
+        DSC[n] = DSC[n]:gsub(k, API.REPLACE[k]:gsub(API.REPLACE.__key, k))
+      end
+    end
+    table.insert(t, n)
+  end
+end
+
+function wikilib.printTypeReference(API)
+  local tT = API.TYPE.list
+  local sL = API.TYPE.link
+  local sT = API.TYPE.__tfm
+  local fR = API.TYPE.__rbr
+  for ID = 1, #tT do
+    io.write("\n"..fR:format(tT[ID][1], sL:format(sT:format(tT[ID][1]))))
+  end; io.write("\n")
+end
+
+function wikilib.printRow(tT)
+  io.write("\n|"..table.concat(tT, "|").."|")
+end
+
+--[[
+  sT > Text to process
+  bP > Enable pictures
+  bD > Dicable and return empry string
+]]--
+function wikilib.concatType(API, sT, bP, bD)
+  if(bD) then return "" end
+  local sV = tostring(sT)
+  if(sV:sub(1,1) == "/") then sV = sV:sub(2,-1) end
+  bU = common.getPick(bP ~= nil, bP, API.TYPE.__pic)
+  if(bU) then
+    local sL = API.TYPE.link
+    local sI = API.TYPE.__img
+    local sR = API.TYPE.__rty
+    local exp = common.stringExplode(sV, "/")
+    for iN = 1, #exp do
+      exp[iN] = sI:format(sR:format(exp[iN]))
+    end
+    return table.concat(exp, ",")
+  else
+    return sV
+  end
+end
+
+function wikilib.readReturnValues(API)
+  local sN = tostring(API.FILE.base)..tostring(API.FILE.path)
+  if(sN:sub(-1,-1) ~= "/") then sN = sN.."/" end
+  sN = sN..API.TYPE.E2:lower().."_rt.txt"
+  local fR = io.open(sN, "r")
+  if(not fR) then return end
+  local sL = fR:read("*line")
+  while(sL ~= nil) do
+    local sT = common.stringTrim(sL)
+    if(sL ~= "") then
+      local tT = common.stringExplode(sT, ":")
+      tT[1] = common.stringTrim(tostring(tT[1]))
+      tT[2] = common.stringTrim(tostring(tT[2]))
+      print("wikilib.readReturnValues", tT[1], tT[2])
+      API.RETURN.MATCH[tT[1]] = tT[2]
+    end
+    sL = fR:read("*line")
+  end
+end
+
+function wikilib.convTypeE2Description(API, sT)
+  local tTyp = API.TYPE; return tTyp.list[tTyp.idx[sT]][1]
+end
+
+-- e2function stcontrol stcontrol:setPower(number nP, number nI, number nD)
+function wikilib.convApiE2Description(API, sE2)
+  local sE = common.stringTrim(sE2)
+  if(sE:sub(1,10) == "e2function") then
+    local tInfo, tTyp = {}, API.TYPE
+    sE = common.stringTrim(sE:sub(11, -1))
+    iS = sE:find("%s", 1)
+    tInfo.ret = wikilib.convTypeE2Description(API,common.stringTrim(sE:sub(1, iS)))
+    sE = common.stringTrim(sE:sub(iS, -1))
+    iS = sE:find(":", 1, true)
+    if(iS) then
+      tInfo.obj = wikilib.convTypeE2Description(API,common.stringTrim(sE:sub(1, iS-1)))
+      sE   = common.stringTrim(sE:sub(iS+1, -1))     
+    end
+    iS = sE:find("(", 1, true)
+    tInfo.foo = common.stringTrim(sE:sub(1, iS-1))
+    sE = common.stringTrim(sE:match("%(.-%)")):sub(2,-2)
+    tInfo.par = common.stringExplode(sE, ",")
+    for ID = 1, #tInfo.par do
+      tInfo.par[ID] = common.stringTrim(tInfo.par[ID])
+      iS = tInfo.par[ID]:find(" ", 1, true)
+      if(not iS) then break end
+      tInfo.par[ID] = wikilib.convTypeE2Description(API,tInfo.par[ID]:sub(1, iS-1))
+    end; tInfo.com = tInfo.foo.."("
+    if(tInfo.obj) then
+      tInfo.com = tInfo.com..tInfo.obj..":" end
+    for ID = 1, #tInfo.par do
+      tInfo.com = tInfo.com..tInfo.par[ID]
+    end; tInfo.com = tInfo.com..")"; return tInfo
+  end; return nil
+end
+
+function wikilib.makeReturnValues(API)
+  local sN = tostring(API.FILE.base)..tostring(API.FILE.slua)
+  if(sN:sub(-1,-1) ~= "/") then sN = sN.."/" end
+  sN = sN..API.TYPE.E2:lower()..".lua"
+  local fR = io.open(sN, "r")
+  if(not fR) then return logStatus("wikilib.makeReturnValues: No file <"..sN..">") end
+  local sL, tF = fR:read("*line"), API.RETURN.MATCH
+  while(sL ~= nil) do
+    local sT = common.stringTrim(sL)
+    if(sL:find("e2function")) then
+      local tL = common.stringExplode(sL, " ")
+      local typ, foo = tL[2], tL[3]
+      local mth = (foo:find(":") or  0)
+      local brk = (foo:find("%(") or -1)
+      foo = foo:sub(mth+1, brk-1)
+      local tP = tF[foo]
+      if(not tP) then tF[foo] = {__top = 0, __key = {}}; tP = tF[foo] end
+      local idx = API.TYPE.idx[typ]
+      local itp = API.TYPE.list[idx][1]
+      tP.__top = tP.__top + 1
+      local tInfo = wikilib.convApiE2Description(API, sL)
+      tP.__key[tInfo.com] = tP.__top
+      tP[tP.__top] = tInfo
+    end
+    sL = fR:read("*line")
+  end; return tF
+end
+
+function wikilib.printTypeTable(API)
+  local tT = API.TYPE.list
+  local fR = API.TYPE.__img
+  local sR = API.TYPE.__rty
+  wikilib.printRow({"Icon", "Description"})
+  wikilib.printRow({"---", "---"})
+  for ID = 1, #tT do
+    wikilib.printRow({fR:format(sR:format(tT[ID][1])), tT[ID][2]})
+  end; io.write("\n")
+end
+
+function wikilib.printDescriptionTable(API, DSC, iN)
+  local tPool = API.POOL[iN]
+  if(not tPool) then return end   
+  local nC, tC = #tPool.cols, {}
+  local tH = {}; for ID = 1, nC do 
+    tH[ID] = ("-"):rep(common.getClamp((tPool.size[ID] or tPool.cols[ID]:len()), 3))
+    tC[ID] = common.stringCenter(tPool.cols[ID],tPool.size[ID],".")
+  end; table.sort(tPool); tPool.data = {}
+  wikilib.printRow(tC); wikilib.printRow(tH)  
+  for i, n in ipairs(tPool) do
+    local arg, vars, obj = n:match("%(.-%)"), "", ""
+    if(arg) then arg = arg:sub(2,-2)
+      local tsk = common.stringExplode(arg,":")
+      if(not arg:find(":")) then
+        tsk[2], tsk[1] = tsk[1], API.TYPE.__non end
+      if(tsk[2] == "") then tsk[2] = API.TYPE.__non end
+      tsk[1], tsk[2] = common.stringTrim(tsk[1]), common.stringTrim(tsk[2])
+      local k, len = 1, tsk[2]:len(); obj = "/"..tsk[1]
+      while(k <= len) do local sbc = tsk[2]:sub(k,k)
+        if(sbc == "x") then
+          sbc = tsk[2]:sub(k, k+2); k = (k + 2) -- The end of the current type
+        end; k = (k + 1)
+        vars = vars.."/"..sbc
+      end
+    end
+      
+    for rmk, rmv in pairs(API.RETURN.MATCH) do
+      if(n:find(rmk.."%(") and rmv.__top > 0) then
+        local ret = ""; table.sort(rmv, apiSortFinctionParam)
+        for ID = 1, rmv.__top do
+          local api = rmv[ID]; ret = api.ret
+          if(n == api.com) then
+            if(ret == "") then
+              if(n:find(API.NAME)) then
+                ret = "/"..API.TYPE.__obj
+              elseif(cap and API.RETURN.PREF[n:sub(1,cap-1)]) then print(12)
+                ret = "/"..API.RETURN.PREF[n:sub(1,cap-1)]
+              else
+                ret = "/"..API.TYPE.__non
+              end
+            end
+            
+            if(obj:find(API.TYPE.__non)) then
+              wikilib.printRow({n:gsub("%(.-%)", "("..wikilib.concatType(API, vars, true)
+                    ..")"), wikilib.concatType(API, ret, true), DSC[n]})      
+            else
+              wikilib.printRow({wikilib.concatType(API, obj , true)..":"..n:gsub("%(.-%)", "("
+                      ..wikilib.concatType(API, vars, true)..")"), wikilib.concatType(API, ret, true), DSC[n]})
+            end
+          end
+        end
+      end
+    end
+  end
+  io.write("\n")
+end
+
+return wikilib
